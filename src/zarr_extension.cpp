2a09bf9add6dade68fb3119566cf5a34b8845b69
Hobbes Merose <al+andhobbes@merose.com>
2026-03-14 14:57:39 -0700

    Fix format check and rename remaining xql references to zarr
    - Fixed formatting issues in zarr_extension.cpp (indentation)
    - Renamed remaining xql references to zarr:
      - Changed 'XQL' string output to 'zarr' in scalar functions
      - Renamed xql_scalar_function to zarr_scalar_function
      - Renamed xql_openssl_version to zarr_openssl_version
      - Renamed xql_openssl_version_scalar_function to zarr_openssl_version_scalar_function

--- src/zarr_extension.cpp
+++ src/zarr_extension.cpp
@@ -14,27 +14,27 @@ namespace duckdb {
 inline void ZarrScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &name_vector = args.data[0];
 	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "XQL " + name.GetString() + " 🦆");
+		return StringVector::AddString(result, "zarr " + name.GetString() + " 🦆");
 	});
 }
 
 inline void ZarrOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &name_vector = args.data[0];
 	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "XQL " + name.GetString() + ", my linked OpenSSL version is " +
+		return StringVector::AddString(result, "zarr " + name.GetString() + ", my linked OpenSSL version is " +
 		                                           OPENSSL_VERSION_TEXT);
 	});
 }
 
 static void LoadInternal(ExtensionLoader &loader) {
 	// Register a scalar function
-	auto xql_scalar_function = ScalarFunction("zarr", {LogicalType::VARCHAR}, LogicalType::VARCHAR, ZarrScalarFun);
-	loader.RegisterFunction(xql_scalar_function);
+	auto zarr_scalar_function = ScalarFunction("zarr", {LogicalType::VARCHAR}, LogicalType::VARCHAR, ZarrScalarFun);
+	loader.RegisterFunction(zarr_scalar_function);
 
 	// Register another scalar function
-	auto xql_openssl_version_scalar_function = ScalarFunction("xql_openssl_version", {LogicalType::VARCHAR},
-	                                                            LogicalType::VARCHAR, ZarrOpenSSLVersionScalarFun);
-	loader.RegisterFunction(xql_openssl_version_scalar_function);
+	auto zarr_openssl_version_scalar_function = ScalarFunction("zarr_openssl_version", {LogicalType::VARCHAR},
+	                                                           LogicalType::VARCHAR, ZarrOpenSSLVersionScalarFun);
+	loader.RegisterFunction(zarr_openssl_version_scalar_function);
 }
 
 void ZarrExtension::Load(ExtensionLoader &loader) {
